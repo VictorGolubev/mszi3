9677e5f01c213c74f89934551c55cdb8cf2fed9a
Victor <golube4754@gmail.com>
2020-05-27 05:35:05 +0300

    v1

---
+++ PreparedToGPSS/Pirson.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PreparedToGPSS
+{
+    public class Pirson
+    {
+        public  static Dictionary<string,string> getParams(List<double> vector, double sum)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            double MX = getMX(vector);//Мат ожидание
+            double DX = getDX(vector, MX); //Дисперсия
+
+            //Параметры нормального для экспоненциального распределения
+            double u = 1 / MX; //Плотность
+            //Параметры распределение для экспоненциального распределения
+            double a = MX; //Мат ожидание
+            double b = Math.Sqrt(DX); //среднеквадратичное отклонение распределения
+
+            double HiNorm = getHiNormalDistibution(vector, sum, a, b);
+            double HiExp = getHiExpDistibution(vector, sum, u);
+            if (HiNorm < HiExp)
+                result.Add("Norm", "a = " + a + " b = " + b);
+            else
+                result.Add("Exp", "u = " + u);
+            
+            return result;
+        }
+        private static double getHiNormalDistibution(List<double> vector, double sum, double a, double b)
+        {
+            List<double> distribution = getNormalDistribution(vector, sum, a, b);
+            double H = 0;
+            for (int i = 0; i < distribution.Count; i++)
+            {
+                double temp;
+                temp = (distribution[i] - vector[i]) * (distribution[i] - vector[i]) / distribution[i]; // вес i-го разряда на меру отклонения Пирсонаа (Отклонение от ожидаемого в квадрате)
+                H += temp;
+            }
+            return H;
+        }
+        private static double getHiExpDistibution(List<double> vector, double sum, double u)
+        {
+            List<double> distribution = getExpDistribution(vector, sum, u);
+            double H = 0;
+            for (int i = 0; i < distribution.Count; i++)
+            {
+                double temp;
+                temp = (distribution[i] - vector[i]) * (distribution[i] - vector[i]) / distribution[i]; // вес i-го разряда на меру отклонения Пирсонаа (Отклонение от ожидаемого в квадрате)
+                H += temp;
+            }
+            return H;
+        }
+
+        private static List<double> getNormalDistribution(List<double> vector, double sum, double a, double b)
+        {
+
+            //Результат
+            List<double> result = new List<double>();
+
+            foreach (int temp in vector)
+                result.Add(normalD(temp, a, b) * sum);
+
+            return result;
+
+
+
+        }
+
+        private static List<double> getExpDistribution(List<double> vector, double sum, double u)
+        {
+
+            //Результат
+            List<double> result = new List<double>();
+
+            foreach (int temp in vector)
+                result.Add(expD(temp, u) * sum);
+
+            return result;
+
+
+
+        }
+
+        private static double getMX(List<double> vector)
+        {
+            double sum = 0;
+            for (int i = 0; i < vector.Count; i++)
+                sum += vector[i];
+            return sum / vector.Count;
+        }
+
+        private static double getDX(List<double> vector, double MX)
+        {
+            double sum = 0;
+            for (int i = 0; i < vector.Count; i++)
+                sum += Math.Pow(vector[i] - MX, 2);
+            return sum / vector.Count;
+        }
+
+        //Расчет по формуле нормального распределения
+        private static double normalD(double X, double a, double b)
+        {
+            double res = 1 / (b * Math.Sqrt(2 * Math.PI));
+            double power = (-1) * (X - a) * (X - a) / (2 * b * b);
+            return res * Math.Exp(power);
+        }
+
+        private static double expD(double x, double u)
+        {
+            if (x >= 0)
+                return u * Math.Exp((-1) * u * x);
+            else
+                return 0;
+        }
+
+    }
+}
---
+++ PreparedToGPSS/Program.cs
@@ -0,0 +1,174 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using Tutorial.SqlConn;
+using PreparedToGPSS;
+
+
+namespace ConnectMySQL
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> tableNameAndVectors = new Dictionary<string, List<string>>();
+
+            Console.WriteLine("Getting Connection ...");
+            MySqlConnection conn = DBUtils.GetDBConnection();
+
+            //Открытие соединения с БЛ
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+
+                //Запрос на использование схемы БД
+                string sqlQuery = "USE dump_rasulov";
+                MySqlCommand command = new MySqlCommand(sqlQuery, conn);
+                command.ExecuteNonQuery();
+
+                sqlQuery = "CREATE TABLE `params_for_distribution` (`id` INT NOT NULL AUTO_INCREMENT, `table_name` VARCHAR(450) NULL, `type_of_distributions` VARCHAR(450) NULL, `params` VARCHAR(450) NULL, PRIMARY KEY(`id`));";
+                command = new MySqlCommand(sqlQuery, conn);
+                command.ExecuteNonQuery();
+
+
+                //Получение таблиц для которых будут подсчитаны векторы
+                string sqlQuerySelectTebleName = "SELECT table_name FROM sample;";
+                command = new MySqlCommand(sqlQuerySelectTebleName, conn);
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    string tableName = reader[0].ToString();
+                    tableNameAndVectors.Add(tableName, new List<string>());
+                }
+                reader.Close();
+
+                //Получение порогового значения и векторов
+                foreach (KeyValuePair<string, List<string>> entry in tableNameAndVectors)
+                {
+                    string tableName = entry.Key;
+                    string queryCount = getQueryCountInTable(tableName);
+                    command = new MySqlCommand(queryCount, conn);
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+                    //Если в таблице меньше 100 пакетов то и 100 и более групп невозможно получить
+                    List<DateTime> dateTimes = new List<DateTime>(); // Время всех пакетов с соблюдением чередности
+                    List<TimeSpan> listForVector = new List<TimeSpan>(); // параметр промежуток между пакетами
+
+                    string queryTimeStamps = getQueryTimeStampsFromTable(tableName);
+                    command = new MySqlCommand(queryTimeStamps, conn);
+                    //string beginTime = "00:00:00";
+                    string beginTime = command.ExecuteScalar().ToString();
+                    DateTime tempTime = DateTime.Parse(beginTime);
+                    reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string current = reader[0].ToString();
+                        DateTime currentTime = DateTime.Parse(current);
+
+                        dateTimes.Add(currentTime);
+
+                        insertToSortedList(listForVector, currentTime - tempTime);
+                        tempTime = currentTime;
+                    }
+                    reader.Close();
+
+                    listForVector.Reverse(); // Теперь промежутки между пакетами идут на убывание
+
+                    //Пороговое значение
+                    TimeSpan limitValue = listForVector[100];
+                    if (limitValue.CompareTo(new TimeSpan(0)) != 0)
+                    {
+                        List<double> durationGroups = new List<double>();//последовательность длительности групп
+                        List<double> spanGroups = new List<double>();//последовательность интервалов между группами
+                        double sumDuration = 0;
+                        double sumSpan = 0;
+
+                        DateTime beginBlock = dateTimes[0];
+                        DateTime endBlock = dateTimes[0];
+                        DateTime newBeginBlock = dateTimes[0];
+                        for (int i = 1; i < dateTimes.Count; i++)
+                        {
+                            TimeSpan tempSpan = dateTimes[i] - dateTimes[i - 1];
+                            if (tempSpan <= limitValue)
+                            {
+                                //Если интервал не превышает заданное пороговое значение
+                                continue;
+                            }
+                            else
+                            {
+                                //Если интервал превышает заданное пороговое значение
+                                endBlock = dateTimes[i - 1];
+                                double temp = (endBlock - beginBlock).TotalMilliseconds;
+                                durationGroups.Add(temp);
+                                sumDuration += temp;
+                                temp = (newBeginBlock - beginBlock).TotalMilliseconds;
+                                spanGroups.Add(temp);
+                                sumSpan += temp;
+                                beginBlock = dateTimes[i];
+                            }
+                        }
+
+                        //Тут у нас уже есть два вектора durationGroups и spanGroups
+                        Dictionary<string, string> parametrs = Pirson.getParams(durationGroups, sumDuration);
+                        foreach (KeyValuePair<string, string> temp in parametrs)
+                        {
+                            string queryInsert = getQueryInsert("Длит.Блока "+tableName, temp.Key, temp.Value);
+                            command = new MySqlCommand(queryInsert, conn);
+                            command.ExecuteNonQuery();
+                        }
+                        parametrs = Pirson.getParams(spanGroups, sumSpan);
+                        foreach (KeyValuePair<string, string> temp in parametrs)
+                        {
+                            string queryInsert = getQueryInsert("Длит.Инт. " + tableName, temp.Key, temp.Value);
+                            command = new MySqlCommand(queryInsert, conn);
+                            command.ExecuteNonQuery();
+                        }
+
+
+                    }
+                }
+
+                //Выполнение без ошибок
+                Console.WriteLine("End...");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally
+            {
+                //Закрытие соединенися с БЛ
+                conn.Close();
+            }
+
+            Console.Read();
+        }
+        private static string getQueryCountInTable(string tableName)
+        {
+            return "SELECT COUNT(*) FROM `" + tableName + "`;";
+        }
+        private static string getQueryTimeStampsFromTable(string tableName)
+        {
+            return "SELECT time_stamp FROM `" + tableName + "`;";
+        }
+
+        private static void insertToSortedList(List<TimeSpan> list, TimeSpan value)
+        {
+            if (list.Count == 0)
+                list.Add(value);
+            else
+            {
+                int i = 0;
+                while (i != list.Count && value > list[i])
+                    i++;
+                list.Insert(i, value);
+            }
+        }
+        private static string getQueryInsert(string tableName, string type, string parametrs)
+        {
+            return "INSERT INTO `params_for_distribution` (table_name, type_of_distributions, params) VALUES ('" + tableName + "','" + type + "','" + parametrs + "');";
+        }
+    }
+}
